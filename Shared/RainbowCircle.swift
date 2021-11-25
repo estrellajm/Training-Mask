a9bad5e98d0978ea54474d3dc87c7d50d3baac14
Jose Estrella <estrellajosem@gmail.com>
2021-11-25 14:59:11 -0700

    added rainbow circle

--- Shared/RainbowCircle.swift
+++ Shared/RainbowCircle.swift
@@ -8,35 +8,46 @@
 import SwiftUI
 
 struct RainbowCircle: View {
-    @State var progressValue: Float = 38.0
+    @State var progressValue: Float = 2.0
     
-    var lineWidth: CGFloat = CGFloat(50)
+    var lineWidth: CGFloat = CGFloat(20)
+    var trimLine: CGFloat = CGFloat(0.7)
     
     private let gradient = AngularGradient(
-        gradient: Gradient(colors: [Color.blue, .red, .yellow, .white]),
+        gradient: Gradient(colors: [.blue, .red, .orange]),
         center: .center,
-        startAngle: .degrees(270),
-        endAngle: .degrees(0))
+        startAngle: .degrees(389),
+        endAngle: .degrees(180)
+    )
     
     var body: some View {
         ZStack {
-            Color.yellow
-                .opacity(0.1)
-                .edgesIgnoringSafeArea(.all)
-            VStack {
-                ProgressBar(progress: self.$progressValue)
-                    .frame(width: 150.0, height: 150.0)
-                    .padding(20.0)
-            }
+            //            Color.yellow
+            //                .opacity(0.1)
+            //                .edgesIgnoringSafeArea(.all)
+            //            VStack {
+            //                ProgressBar(progress: self.$progressValue)
+            //                    .frame(width: 150.0, height: 150.0)
+            //                    .padding(20.0)
+            //            }
             Spacer()
-            Circle().stroke(Color.white, lineWidth: lineWidth)
+            Circle().stroke(.blue.opacity(0.1), lineWidth: lineWidth)
             Circle()
-                .trim(from: 0, to: CGFloat(0.8))
+                .trim(from: 0, to: trimLine)
+                .rotation(Angle.degrees(145))
                 .stroke(gradient, style: StrokeStyle(lineWidth: lineWidth, lineCap: .round))
-                .overlay(
-                    Circle().trim(from: 0, to: CGFloat(0.8))
-                        .rotation(Angle.degrees(-4))
-                        .stroke(gradient, style: StrokeStyle(lineWidth: lineWidth, lineCap: .butt)))
+//                .overlay(
+//                    Circle()
+//                        .trim(from: 0, to: trimLine)
+//                        .rotation(Angle.degrees(270))
+//                        .stroke(gradient, style: StrokeStyle(lineWidth: lineWidth, lineCap: .butt))
+//                        .overlay(
+//                            Circle()
+//                                .trim(from: 0, to: trimLine)
+//                                .rotation(Angle.degrees(180))
+//                                .stroke(gradient, style: StrokeStyle(lineWidth: lineWidth, lineCap: .round))
+//                        )
+//                )
             
         }
         .padding(60)
@@ -57,7 +68,7 @@ struct ProgressBar: View {
                 .trim(from: 0.0, to: CGFloat(min(self.progress, 1.0)))
                 .stroke(style: StrokeStyle(lineWidth: 20.0, lineCap: .round, lineJoin: .round))
                 .foregroundColor(Color.red)
-                .rotationEffect(Angle(degrees: 270.0))
+                .rotationEffect(Angle(degrees: 180.0))
             //                .animation(.linear, value: self.progress)
             Text(String(format: "%.0f %%", min(self.progress, 1.0)*100.0))
                 .font(.largeTitle)
